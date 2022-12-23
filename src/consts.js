0d8dbc448976abb857c8de7ce01abcbe08710bc6
Sierra Acy <sierra.acy@gmail.com>
2022-12-23 00:08:15 -0800

    populate stats based on trope

--- src/consts.js
+++ src/consts.js
@@ -1,39 +1,42 @@
-const Tropes = [
+const TropeOptions = [
     {
         "text": "",
         "value": "no-choice",
-        "stats": {
-            "fight": "",
-            "flight": "",
-            "brains": "",
-            "brawn": "",
-            "charm": "",
-            "grit": ""
-        },
         "selected": true
     },
     {
         "text": "Aloof Teacher",
-        "value": "aloof-teacher",
-        "stats": {
-            "fight": "",
-            "flight": "",
-            "brains": "",
-            "brawn": "",
-            "charm": "",
-            "grit": ""
-        }
+        "value": "aloof-teacher"
     },
     {
         "text": "Bullheaded Muscle",
-        "value": "bullheaded-muscle",
-        "stats": {
-            "fight": "",
-            "flight": "",
-            "brains": "",
-            "brawn": "",
-            "charm": "",
-            "grit": ""
-        }
+        "value": "bullheaded-muscle"
     }
 ];
+
+const TropeStats = {
+    "no-choice": {
+        "fight": "",
+        "flight": "",
+        "brains": "",
+        "brawn": "",
+        "charm": "",
+        "grit": ""
+    },
+    "aloof-teacher": {
+        "fight": "d4",
+        "flight": "d20",
+        "brains": "d12",
+        "brawn": "d6",
+        "charm": "d8",
+        "grit": "d10"
+    },
+    "bullheaded-muscle": {
+        "fight": "d12",
+        "flight": "d8",
+        "brains": "d6",
+        "brawn": "d20",
+        "charm": "d4",
+        "grit": "d10"
+    }  
+}
--- src/index.js
+++ src/index.js
@@ -9,9 +9,20 @@ function init() {
 
 function initTropeOptions() {
   let optionList = document.getElementById('tropes').options;
-  Tropes.forEach(option =>
+  TropeOptions.forEach(option =>
     optionList.add(
-      new Option(option.text, option.value, selected=option.selected ? option.selected : false)
+      new Option(option.text, option.value, defaultSelected=option.selected ? option.selected : false)
     )
   );
+}
+
+function tropeSelect(selectedValue) {
+  if (selectedValue == 'no-choice') {
+    alert("Please choose a trope.");
+  }
+  const tropeStatsData = TropeStats[selectedValue];
+  Object.entries(tropeStatsData).forEach(([key, value]) => {
+    let statField = document.getElementById(key);
+    statField.value = value;
+  });
 }
